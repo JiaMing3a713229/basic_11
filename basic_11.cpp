d84541aed0aee8b38cbe6618813666c18aae2aba
JiaMing <ty10410205@gmail.com>
2019-03-30 17:12:01 +0800

    version1

--- basic_11.cpp
+++ basic_11.cpp
@@ -2,19 +2,4 @@
 #include<stdlib.h>
 int main(void){
 	int x,y,z,tmp;
-	printf("½Ð¿é¤Jx,y,z");
-	scanf("%d%d%d",&x,&y,&z);
-	int array[3]={x,y,z};
-	for(int i=0;i<2;i++)
-		for(int j=i+1;j<3;j++){
-			if(array[j]>array[i]){
-				tmp=array[i];
-				array[i]=array[j];
-				array[j]=tmp;	
-			}
-		}
-	for(int i=2;i>=0;i--){
-		printf("%d",array[i]);
-		printf(" ");
-	}
-} 
+	printf("
